cf5284c6841c930eaddf23e9111f1e21a5742824
javasunhao <849142551@qq.com>
2019-06-16 20:48:16 +0800

    上证50股票数据出理

--- src/test/java/com/sunhao/stockprojecteg/MyTest.java
+++ src/test/java/com/sunhao/stockprojecteg/MyTest.java
@@ -12,9 +12,8 @@ public class MyTest {
     @Test
     public void test01() {
         List<String> stockCodeList = new ArrayList<>();
-        stockCodeList.add("cn_000002");
-        stockCodeList.add("cn_601006");
-        String str = DataFromNetUtils.getStockJsonData(stockCodeList, "20190528", "20190529");
+        stockCodeList.add("cn_000001");
+        String str = DataFromNetUtils.getStockJsonData(stockCodeList, "20190604", "20190604");
         System.out.println(str);
         DataFromNetUtils.getObjectByStr(str,"codeName");
         //historySearchHandler([{"status":0,"hq":[["2018-10-30","23.95","23.80","-0.16","-0.67%","22.70","24.45","242465","57460.49","1.28%"]],"code":"cn_600588","stat":["锟桔硷拷:","2018-10-30锟斤拷2018-10-30","-0.16","-0.67%",22.7,24.45,242465,57460.49,"1.28%"]}])
